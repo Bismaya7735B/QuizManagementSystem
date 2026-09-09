1dbc88fdf673ae97a6691df2a8a3688a5682c773
Bismaya7735B <96188267+Bismaya7735B@users.noreply.github.com>
2026-09-09 12:35:42 +0530

    Update V7

--- app.py
+++ app.py
@@ -1515,9 +1515,6 @@ def candidate_dashboard():
                     finalize_exam(user_answers, is_late=True)
                     return
 
-            finalize_exam(user_answers, is_vi_exam(user_answers, is_late=True))
-                    return
-
             finalize_exam(user_answers, is_violation=is_tab_violation)
 
 
