8617603d578fc495230ec5993ee1236934df851c
Bismaya7735B <96188267+Bismaya7735B@users.noreply.github.com>
2026-09-09 12:23:21 +0530

    Update on Tab Switch V3

--- app.py
+++ app.py
@@ -1518,7 +1518,7 @@ def candidate_dashboard():
             finalize_exam(user_answers, is_vi_exam(user_answers, is_late=True)
                     return
 
-            finalize_exam(user_answers, is_violation=is_tab_violation)
+            
 
 
 # --- ROUTER ---
